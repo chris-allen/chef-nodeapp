f7e0d37fdb76c152392d1f64a4519b6e1d3f47da
chris-allen <theone.theonly.me@gmail.com>
2017-01-15 10:42:19 -0500

    First commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-nodeapp'
+maintainer       'Chris Allen'
+maintainer_email 'chris@apaxsoftware.com'
+license          'All rights reserved'
+description      'Installs/Configures chef-nodeapp'
+long_description 'Installs/Configures chef-nodeapp'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef-nodeapp
+# Recipe:: default
+#
+# Copyright (C) 2017 Chris Allen
+#
+# All rights reserved - Do Not Redistribute
+#

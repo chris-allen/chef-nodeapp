f1266831bd83988bb210a39ba1a732cecf22f1d6
chris-allen <theone.theonly.me@gmail.com>
2017-01-24 11:41:54 -0500

    Switching to express

--- recipes/system.rb
+++ recipes/system.rb
@@ -15,9 +15,8 @@ end
 
 package "git"
 
+node.set['nodejs']['install_method'] = 'binary'
+node.set['nodejs']['version'] = '6.9.2'
+node.set['nodejs']['binary']['checksum'] = '997121460f3b4757907c2d7ff68ebdbf87af92b85bf2d07db5a7cb7aa5dae7d9'
 include_recipe "nodejs::npm"
-execute "upgrade_npm" do
-  command "sudo npm -g install npm@latest"
-  action :run
-end
-nodejs_npm "sails"
+nodejs_npm "express-generator"
